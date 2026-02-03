9f2b5726b1b7567d5cb50f6a9fb3091ebe92fb38
xM0Se <189546389+xM0Se@users.noreply.github.com>
2026-02-03 15:26:41 +0100

    i hope combos work now :]

--- qmk/combos.c
+++ qmk/combos.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "combos.h"
 #include "layers.h"
 #include "tap-dances.h"
 
@@ -7,7 +8,7 @@ const uint16_t PROGMEM homerow_c_k[] = {KC_C, KC_K, COMBO_END};
 const uint16_t PROGMEM homerow_scroll_g_m[] = {KC_G, KC_M, COMBO_END};
 const uint16_t PROGMEM tab_shift_space[] = {TD(TD_SFT_TT2), TD(TD_SPC_ENT_TT1), COMBO_END};
 const uint16_t PROGMEM delete_shift_bspc[] = {TD(TD_SFT_TT2), TD(TD_ALT_BSPC_TT3), COMBO_END};
-combo_t key_combos[] = {
+combo_t key_combos[COMBO_LENGTH] = {
     COMBO(homerow_c_k, MEH(KC_C)),
     COMBO(homerow_scroll_g_m, MEH(KC_G)),
     COMBO(tab_shift_space, KC_TAB),
---
+++ qmk/combos.h
@@ -0,0 +1,10 @@
+#pragma once
+
+enum combos {
+    ESC,
+    HOMEROW,
+    HOMEROW_SCROLL,
+    TAB,
+    DELETE,
+    COMBO_LENGTH
+};
